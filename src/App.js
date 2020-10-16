3cb428e0783003284e5c96f6080c596d71d015db
Victor Trajano <trajanodev@hotmail.com>
2020-10-16 12:21:57 -0400

    Finalização frontEnd

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/header'
 import './styles.css'
 import Routes from './routes'
+import Footer from './components/footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Routes/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+
+    const Footer  = ()  => 
+    
+    <footer id="main-footer"> 
+    
+    <p>Feito por: Victor Trajano</p>
+    <p>Contato: (92)98212-7867</p>
+    <p>Email: trajanodev@hotmail.com</p>
+
+    </footer>
+    
+    
+export default Footer;
